60749726d2feabb78a4a6948ed935b60bb37f92b
GuilhermeMartins13 <guilhermemartinssenai@gmail.com>
2019-06-07 16:38:16 -0300

    Interface

--- src/main/java/com/GuilhermeMartins/domain/Endereco.java
+++ src/main/java/com/GuilhermeMartins/domain/Endereco.java
@@ -29,7 +29,15 @@ public class Endereco implements Serializable {
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
 	
-	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep, Cliente cliente) {
+	@ManyToOne
+	@JoinColumn(name="cidade_id")
+	private Cidade cidade;
+	
+	public Endereco () {
+		
+	}
+	
+	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep, Cliente cliente, Cidade cidade) {
 		super();
 		this.id = id;
 		this.logradouro = logradouro;
@@ -38,6 +46,7 @@ public class Endereco implements Serializable {
 		this.bairro = bairro;
 		this.cep = cep;
 		this.cliente = cliente;
+		this.cidade = cidade;
 		
 	}
 
@@ -95,6 +104,15 @@ public class Endereco implements Serializable {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
 	}
 
 	@Override
